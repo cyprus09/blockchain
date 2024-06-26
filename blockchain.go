25e793232d79e1b29406fdb144ac2df800f3ed41
mayankpallai <mayankpallai@gmail.com>
2024-06-27 02:51:48 +0800

    implement db and change logic to iterate through the blockchain and add through voting

--- blockchain.go
+++ blockchain.go
@@ -1,18 +1,135 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/boltdb/bolt"
+)
+
+const (
+	dbFile       = "blockchain.db"
+	blocksBucket = "blocks"
+)
+
 // Blockchain keeps a sequence of Blocks in the blockchain
 type Blockchain struct {
-	blocks []*Block
+	tip []byte
+	db  *bolt.DB
+}
+
+type BlockchainIterator struct {
+	currHash []byte
+	db       *bolt.DB
 }
 
 // AddBlock saves the provided data as a block in the blockchain
 func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.CurrHash)
-	bc.blocks = append(bc.blocks, newBlock)
+	var lastHash []byte
+
+	err := bc.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	newBlock := NewBlock(data, lastHash)
+
+	err = bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		err := b.Put(newBlock.CurrHash, newBlock.SerializeBlock())
+		if err != nil {
+			log.Panic(err)
+		}
+
+		err = b.Put([]byte("l"), newBlock.CurrHash)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		bc.tip = newBlock.CurrHash
+
+		return nil
+	})
+}
+
+// Iterator for the blocks in the blockchain
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator{bc.tip, bc.db}
+
+	return bci
+}
+
+// NextBlock returns the next block starting from the tip
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currHash)
+		block = DeserializeBlock(encodedBlock)
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return block
 }
 
 // NewBlockChain creates a new Blockchain with the genesis block
 func NewBlockChain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+	var tip []byte
+
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b == nil {
+			fmt.Println("No existing blockchain found. Initialising a new one...")
+			genesis := NewGenesisBlock()
+
+			b, err := tx.CreateBucket([]byte(blocksBucket))
+			if err != nil {
+				log.Panic(err)
+			}
+
+			err = b.Put(genesis.CurrHash, genesis.SerializeBlock())
+			if err != nil {
+				log.Panic(err)
+			}
+
+			err = b.Put(genesis.CurrHash, genesis.SerializeBlock())
+			if err != nil {
+				log.Panic(err)
+			}
+
+			err = b.Put([]byte("l"), genesis.CurrHash)
+			if err != nil {
+				log.Panic(err)
+			}
+			tip = genesis.CurrHash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bc := Blockchain{tip, db}
+
+	return &bc
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module project/blockchain
 
 go 1.22.2
+
+require github.com/boltdb/bolt v1.3.1
+
+require golang.org/x/sys v0.21.0 // indirect
