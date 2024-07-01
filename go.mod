2d6e7af48e10163a76e237f3366aa764e4c221fe
mayankpallai <mayankpallai@gmail.com>
2024-07-02 02:04:55 +0800

    implement single wallet structure

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.22.2
 
 require github.com/boltdb/bolt v1.3.1
 
-require golang.org/x/sys v0.21.0 // indirect
+require (
+	golang.org/x/crypto v0.24.0
+	golang.org/x/sys v0.21.0 // indirect
+)
--- transaction.go
+++ transaction.go
@@ -168,31 +168,6 @@ func (tx *Transaction) Verify(prevTXs map[string]Transaction) bool {
 	return true
 }
 
-// // SetId sets the transaction ID
-// func (tx *Transaction) SetId() {
-// 	var encoded bytes.Buffer
-// 	var hashValue [32]byte
-
-// 	enc := gob.NewEncoder(&encoded)
-// 	err := enc.Encode(tx)
-// 	if err != nil {
-// 		log.Panic(err)
-// 	}
-
-// 	hashValue = sha256.Sum256(encoded.Bytes())
-// 	tx.ID = hashValue[:]
-// }
-
-// // CanUnlockOutputWith checks whether the address initiated the transaction
-// func (in *TxInput) CanUnlockOutputWith(unlockingData string) bool {
-// 	return in.ScriptSig == unlockingData
-// }
-
-// // CanBeUnlockedWith checks if the output can be unlocked with the provided data
-// func (out *TxOutput) CanBeUnlockedWith(unlockingData string) bool {
-// 	return out.ScriptPubKey == unlockingData
-// }
-
 // NewCoinbaseTx creates a new coinbase transaction
 func NewCoinbaseTx(to, data string) *Transaction {
 	if data == "" {
@@ -213,7 +188,14 @@ func NewUTXOTTransaction(from, to string, amount int, bc *Blockchain) *Transacti
 	var inputs []TxInput
 	var outputs []TxOutput
 
-	acc, validOutputs := bc.FindSpendableOutputs(from, amount)
+	wallets, err := NewWallets()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	wallet := wallets.GetWallet(from)
+	pubKeyHash := HashPubKey(wallet.PublicKey)
+	acc, validOutputs := bc.FindSpendableOutputs(pubKeyHash, amount)
 
 	if acc < amount {
 		log.Panic("ERROR: Not enough funds in your account")
@@ -227,20 +209,21 @@ func NewUTXOTTransaction(from, to string, amount int, bc *Blockchain) *Transacti
 		}
 
 		for _, out := range outs {
-			input := TxInput{txId, out, from}
+			input := TxInput{txId, out, nil, wallet.PublicKey}
 			inputs = append(inputs, input)
 		}
 	}
 
 	// Build a list of outputs
-	outputs = append(outputs, TxOutput{amount, to})
+	outputs = append(outputs, *NewTxOutput(amount, to))
 	if acc > amount {
 		// generate change
-		outputs = append(outputs, TxOutput{acc - amount, from})
+		outputs = append(outputs, *NewTxOutput(acc-amount, from))
 	}
 
 	tx := Transaction{nil, inputs, outputs}
-	tx.SetId()
+	tx.ID = tx.HashValue()
+	bc.SignTransaction(&tx, wallet.PrivateKey)
 
 	return &tx
 }
